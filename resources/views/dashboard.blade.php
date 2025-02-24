b3beb8b7f86837a33914093e638caecb533e1800
NerdLance <101930566+NerdLance@users.noreply.github.com>
2025-02-24 13:05:14 -0500

    Updated Dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,9 @@
 <x-layouts.app>
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+        <div class="">
+            <h3 class="text-2xl font-bold">Welcome to Cloud Party!</h3>
+            <p class="mt-2">Happy Coding!</p>
+        </div>
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
